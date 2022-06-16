bb43627507da22c88a2be9e3393132565d400724
Shin Thant <shinthant10684@gmail.com>
2022-06-16 17:32:33 +0630

    finish project

---
+++ pages/[product].js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useRouter } from "next/router";
+import styles from "../styles/Home.module.css";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+export default function Product() {
+    const {
+        query: { product },
+    } = useRouter();
+
+    return (
+        <div className={styles.detailContainer}>
+            <motion.img
+                layoutId={product}
+                className={styles["big-image"]}
+                src={product + ".jpg"}
+                alt={product}
+            />
+
+            <div className={styles.content}>
+                <h1 className={styles.headText}>{product.toUpperCase()}</h1>
+                <p className={styles.text}>
+                    <p className={styles.highlight}>{product.toUpperCase()}</p>{" "}
+                    is one of the premium products in our store.
+                </p>
+                <Link href="/">
+                    <button className={styles.backBtn}>Back</button>
+                </Link>
+            </div>
+        </div>
+    );
+}
